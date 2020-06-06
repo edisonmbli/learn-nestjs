ce4303bb4b841fda75ca78b64fd6c2023d1b8281
edisonmbli <edisonmbli@gmail.com>
2020-06-07 01:09:03 +0800

    database setting and post module setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './modules/post/post.module';
+import { PostService } from './modules/post/post.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    PostModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PostService],
 })
 export class AppModule {}
